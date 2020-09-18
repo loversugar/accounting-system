612b2b70f4f77fff13bcf52281d012cf7677fc87
loversugar <253262275@qq.com>
2020-09-18 09:08:48 +0200

    update

--- backend/start/main.go
+++ backend/start/main.go
@@ -1,4 +1,14 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main()  {
+	router := gin.Default()
+
+	router.GET("/ping", func(context *gin.Context) {
+		context.Writer.Write([]byte("pong"))
+	})
+
+	router.Run(":8888")
+
 }
